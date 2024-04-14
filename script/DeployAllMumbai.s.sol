e9e884ad4641d392295836f3bd26b2df03e00dda
fabda <fabda@trugly.meme>
2024-04-13 20:44:42 -0700

    Deploy Mumbai

---
+++ script/DeployAllMumbai.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "forge-std/Script.sol";
+
+import {DeploymentsFn} from "./utils/DeploymentsFn.sol";
+import {TruglyVesting} from "../src/TruglyVesting.sol";
+
+contract DeployAllMumbai is Script, DeploymentsFn {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("MNEMONIC_FIRST_ACC_PRIV_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        TruglyVesting vesting = deployVesting();
+        address treasury = deployTreasury();
+        deployMemeception(address(vesting), treasury);
+        deployUniversalRouter(treasury);
+        vm.stopBroadcast();
+    }
+}
---
+++ script/utils/DeploymentsFnMumbai.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "forge-std/Script.sol";
+import {console2} from "forge-std/Test.sol";
+
+import {RouterParameters} from "@trugly-labs/universal-router-fork/base/RouterImmutables.sol";
+import {MumbaiParameters} from "../parameters/Mumbai.sol";
+import {TruglyUniversalRouter} from "../../src/TruglyUniversalRouter.sol";
+import {TruglyVesting} from "../../src/TruglyVesting.sol";
+import {TruglyMemeception} from "../../src/TruglyMemeception.sol";
+
+contract DeploymentsFn is MumbaiParameters {
+    function deployUniversalRouter(address treasury) public returns (TruglyUniversalRouter router) {
+        console2.log("Deploying TruglyUniversalRouter..");
+        RouterParameters memory params = RouterParameters({
+            permit2: PERMIT2,
+            weth9: WETH9,
+            seaportV1_5: UNSUPPORTED_PROTOCOL,
+            seaportV1_4: UNSUPPORTED_PROTOCOL,
+            openseaConduit: UNSUPPORTED_PROTOCOL,
+            nftxZap: UNSUPPORTED_PROTOCOL,
+            x2y2: UNSUPPORTED_PROTOCOL,
+            foundation: UNSUPPORTED_PROTOCOL,
+            sudoswap: UNSUPPORTED_PROTOCOL,
+            elementMarket: UNSUPPORTED_PROTOCOL,
+            nft20Zap: UNSUPPORTED_PROTOCOL,
+            cryptopunks: UNSUPPORTED_PROTOCOL,
+            looksRareV2: UNSUPPORTED_PROTOCOL,
+            routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareToken: UNSUPPORTED_PROTOCOL,
+            v2Factory: V2_FACTORY,
+            v3Factory: V3_FACTORY,
+            pairInitCodeHash: ROUTER_PAIR_INIT_CODE_HASH,
+            poolInitCodeHash: POOL_INIT_CODE_HASH
+        });
+
+        router = new TruglyUniversalRouter(params, treasury);
+        console2.log("Universal Router Deployed:", address(router));
+    }
+
+    function deployVesting() public returns (TruglyVesting vesting) {
+        console2.log("Deploying TruglyVesting..");
+        vesting = new TruglyVesting();
+        console2.log("TruglyVesting Deployed:", address(vesting));
+    }
+
+    function deployTreasury() public pure returns (address) {
+        return TREASURY;
+    }
+
+    function deployMemeception(address vesting, address treasury) public returns (TruglyMemeception memeception) {
+        console2.log("Deploying TruglyMemeception..");
+        memeception = new TruglyMemeception(V3_FACTORY, V3_POSITION_MANAGER, WETH9, vesting, treasury);
+        TruglyVesting(vesting).setMemeception(address(memeception), true);
+        console2.log("TruglyMemeception Deployed:", address(memeception));
+    }
+}
